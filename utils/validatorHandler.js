3f3e1b977eeb53d84be8d37a56d3e6ad269e578f
Koha <khoaendy@gmail.com>
2026-03-13 15:12:52 +0700

    Update

--- utils/validatorHandler.js
+++ utils/validatorHandler.js
@@ -24,11 +24,18 @@ module.exports = {
         body('password').isStrongPassword(options.password).withMessage(`password dai it nhat ${options.password.minLength} ki tu, trong do co it nhat ${options.password.minNumbers} so ${options.password.minUppercase} chu hoa ${options.password.minLowercase} chu thuong ${options.password.minSymbols} ki tu dac biet`),
     ],
     RegisterValidator: [
-        body('email').notEmpty().withMessage("email khong duoc rong").bail().isEmail().withMessage('email sai dinh dang').normalizeEmail(),
-        body('username').notEmpty().isAlphanumeric().withMessage("username khong duoc chua ki tu dac biet"),
-        body('password').notEmpty().withMessage().isStrongPassword(options.password).withMessage(`password dai it nhat ${options.password.minLength} ki tu, trong do co it nhat ${options.password.minNumbers} so ${options.password.minUppercase} chu hoa ${options.password.minLowercase} chu thuong ${options.password.minSymbols} ki tu dac biet`),
-    ]
-    ,
+    body('email').notEmpty().withMessage("email khong duoc rong")
+        .isEmail().withMessage('email sai dinh dang'),
+    body('username').notEmpty().withMessage("username không được rỗng"),
+    body('password').notEmpty().withMessage("password không được rỗng")
+        .isStrongPassword(options.password).withMessage(`password dai it nhat ${options.password.minLength} ki tu...`),
+],
+    changePasswordValidator: [
+        body('oldpassword').notEmpty().withMessage("Mật khẩu cũ không được để trống"),
+        body('newpassword')
+            .isStrongPassword(options.password)
+            .withMessage(`Mật khẩu mới dài ít nhất ${options.password.minLength} kí tự, trong đó có ít nhất ${options.password.minNumbers} số, ${options.password.minUppercase} chữ hoa, ${options.password.minLowercase} chữ thường, ${options.password.minSymbols} kí tự đặc biệt`),
+    ],
     handleResultValidator: function (req, res, next) {
         let result = validationResult(req);
         if (result.errors.length > 0) {
